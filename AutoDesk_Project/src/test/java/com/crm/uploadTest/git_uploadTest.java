c2b31c1fc6643ade2fe5002a04ffe3c4d233b31a
DELL <DELL@AISHWARYA>
2021-05-07 21:56:27 +0530

    New File upload

---
+++ AutoDesk_Project/src/test/java/com/crm/uploadTest/git_uploadTest.java
@@ -0,0 +1,13 @@
+package com.crm.uploadTest;
+
+import org.testng.annotations.Test;
+
+public class git_uploadTest {
+
+	@Test
+	public void generate()
+	{
+		System.out.println("Test script is executed");
+	}
+
+}
